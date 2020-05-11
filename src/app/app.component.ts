a4a145270d6c3d2103e5cc93a9085855e958c5d1
Micah Johnson <mjohnson@teleflora.org>
2020-05-10 19:47:26 -0500

    dist folder added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'payment-form';
+  name: string;
+  dateStr: string;
+  amount: number;
+  miles: number;
+
+  // use the Pipe called 'JSON' to view data on the screen instead of console.log()
+  car = {
+    make: 'nissan',
+    model: 'altima',
+    year: 2020
+  }
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
+
+  onDateChange(value: string) {
+    this.dateStr = value;
+  }
+
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
+
+  onMilesChange(value: string) {
+    this.miles =parseFloat(value);
+  }
 }
